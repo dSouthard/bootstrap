b285ba790bbf9053c41d74445cfdccc953b7e919
Diana <diana.olsen@colorado.edu>
2017-10-12 12:00:27 -0600

    Starting with basic package to test Jenkins

--- src/main/java/com/dsouthard/bootstrap/HelloWorld.java
+++ src/main/java/com/dsouthard/bootstrap/HelloWorld.java
@@ -2,7 +2,7 @@ package com.dsouthard.bootstrap;
 
 public class HelloWorld {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("Hello World");
     }
 }
